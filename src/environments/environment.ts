9963109c2e248aac2a56116cb7665cdf815464c4
core.editor <an.munozq@duocuc.cl>
2023-08-25 02:49:43 -0400

    Login+Navbar

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  firebase: {
+    projectId: 'login-ng-cac4a',
+    appId: '1:346681001192:web:66ed24a75c0dd81a01f55d',
+    storageBucket: 'login-ng-cac4a.appspot.com',
+    apiKey: 'AIzaSyAQfdYg2_ydhoGZBMqRNG1VeIvI8GERlGU',
+    authDomain: 'login-ng-cac4a.firebaseapp.com',
+    messagingSenderId: '346681001192',
+  },
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAQfdYg2_ydhoGZBMqRNG1VeIvI8GERlGU',
+    authDomain: 'login-ng-cac4a.firebaseapp.com',
+    projectId: 'login-ng-cac4a',
+    storageBucket: 'login-ng-cac4a.appspot.com',
+    messagingSenderId: '346681001192',
+    appId: '1:346681001192:web:66ed24a75c0dd81a01f55d',
+  },
+};
